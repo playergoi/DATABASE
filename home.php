c404fbae17876ea014791d2a857675b6b6bf556f
gulshan <gulshanjha311998@gmail.com>
2023-04-12 00:41:55 +0530

    home page added

---
+++ home.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="home.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Oswald:wght@700&display=swap" rel="stylesheet">
+    <title>Document</title>
+</head>
+<body style="background-color: rgb(237, 237, 121);color: rgb(27, 126, 126);background-image: url('http://i.stack.imgur.com/kx8MT.gif');
+background-size: cover;
+height: 100vh;
+">
+ <?php include('_nav.php'); ?>
+ 
+ <h1 class="welcome" ><strong>Welcome to data Seva</strong></h1>
+ <h1 class="animation"><strong>Please Login to order product from our shop...</strong></h1>
+ 
+
+</body>
+</html>
